07216428385856c00fb0e8536286cc755b7d33cd
ZarkaVucetic <zarka_vucetic@yahoo.com>
2018-07-08 17:42:06 +0200

    about pages is added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "donuts#index"
-  
+  get "about/about"
+
   resources :donuts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
